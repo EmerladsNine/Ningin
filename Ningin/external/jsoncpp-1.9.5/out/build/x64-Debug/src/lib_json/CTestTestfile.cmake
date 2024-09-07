1df9bf75a4f3ea0ba3ffed10f82aae360d7d6116
NineEmeralds <107325801+NineEmeralds@users.noreply.github.com>
2024-09-07 18:04:10 +0300

    deleting junk

--- Ningin/external/jsoncpp-1.9.5/out/build/x64-Debug/src/lib_json/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/Downloads/jsoncpp-1.9.5/src/lib_json
-# Build directory: C:/Users/User/Downloads/jsoncpp-1.9.5/out/build/x64-Debug/src/lib_json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
