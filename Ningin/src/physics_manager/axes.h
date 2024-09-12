58161080dee61e1922e55513b0089fcd759b34ef
NineEmeralds <107325801+NineEmeralds@users.noreply.github.com>
2024-09-12 13:00:27 +0300

    some refactoring

--- Ningin/src/physics_manager/axes.h
+++ Ningin/src/physics_manager/axes.h
@@ -9,17 +9,17 @@ enum Axes {
 	XY,
 	XZ,
 	YZ,
-	XYZ,
+	XYZ
 };
 
 Vector3 getDirectionVector(Axes axes) {
     switch (axes) {
-    case X: return Vector3::UNIT_X;
-    case Y: return Vector3::UNIT_Y;
-    case Z: return Vector3::UNIT_Z;
-    case XY: return Vector3::UNIT_XY;
-    case XZ: return Vector3::UNIT_XZ;
-    case YZ: return Vector3::UNIT_YZ;
-    case XYZ: return Vector3::ONE;
+        case X: return Vector3::UNIT_X;
+        case Y: return Vector3::UNIT_Y;
+        case Z: return Vector3::UNIT_Z;
+        case XY: return Vector3::UNIT_XY;
+        case XZ: return Vector3::UNIT_XZ;
+        case YZ: return Vector3::UNIT_YZ;
+        case XYZ: return Vector3::ONE;
     }
 }
