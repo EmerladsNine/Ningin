bfa712566cfafd345a689791acc6df28247b9c51
NineEmeralds <107325801+NineEmeralds@users.noreply.github.com>
2024-09-13 15:13:14 +0300

    removing unnecessary imports + refactoring

--- NinginCore/Behavior.cs
+++ NinginCore/Behavior.cs
@@ -1,4 +1,5 @@
 using System;
+
 namespace NinginCore
 {
     public class Behavior
--- NinginCore/Debug.cs
+++ NinginCore/Debug.cs
@@ -1,7 +1,7 @@
-using System.Runtime.CompilerServices;
 namespace NinginCore
 {
-    public static class Debug{
+    public static class Debug
+    {
         public static void LogInfo(string message) => InternalCalls.DebugLogInfo(message);
         public static void LogWarning(string message) => InternalCalls.DebugLogWarning(message);
         public static void LogError(string message) => InternalCalls.DebugLogError(message);
